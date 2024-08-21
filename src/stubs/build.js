ad064bd98575a22eed040275d0990cb027d49cda
Arturo Rodríguez <arturo@fugadigital.com>
2024-08-20 23:38:42 -0600

    Add stubs

---
+++ src/stubs/build.js
@@ -0,0 +1,4 @@
+import { build } from 'artrz/thunderbird-theme-builder';
+import theme from './themeColors.js';
+
+build(theme, { stylesheet: 'themeCustomStyles.scss' });
---
+++ src/stubs/themeColors.js
@@ -0,0 +1,121 @@
+export default {
+    color_scheme: {
+        // Add your colors here. E.g.:
+        themecol_light_01: '#336699',
+    },
+
+    // All keys are optional
+    theme_colors: {
+        button_background_active:     '', // The color of the background of the pressed toolbar buttons.
+        button_background_hover:      '', // The color of the background of the toolbar buttons on hover.
+        frame:                        '', // The background color of the header area.
+        frame_inactive:               '', // The background color of the header area when the window is inactive.
+        icons:                        '', // The color of the toolbar icons.Defaults to the color specified by toolbar_text.
+        icons_attention:              '', // The color of the toolbar icons in attention state such as the chat icon with new messages.
+        popup:                        '', // The background color of popups such as the AppMenu.
+        popup_border:                 '', // The border color of popups.
+        popup_highlight:              '', // The background color of items highlighted using the keyboard inside popups.
+        popup_highlight_text:         '', // The text color of items highlighted using the keyboard inside popups.
+        popup_text:                   '', // The text color of popups.
+        sidebar:                      '', // The background color of the trees.
+        sidebar_border:               '', // The border color of the trees.
+        sidebar_highlight:            '', // The background color of highlighted rows in trees.
+        sidebar_highlight_border:     '', // The border color of highlighted rows in trees.
+        sidebar_highlight_text:       '', // The text color of highlighted rows in trees.
+        sidebar_text:                 '', // The text color of the trees. Needed to enable the tree theming.
+        tab_background_separator:     '', // The color of the vertical separator of the background tabs.
+        tab_background_text:          '', // The text color of the unselected tabs.
+        tab_line:                     '', // The color of the selected tab line.
+        tab_loading:                  '', // The color of the tab loading indicator.
+        tab_selected:                 '', // Background color of the selected tab. Defaults to the color specified by toolbar.
+        tab_text:                     '', // The text color for the selected tab. Defaults to the color specified by toolbar_text.
+        toolbar:                      '', // The background color of the toolbars. Also used as default value for tab_selected.
+        toolbar_bottom_separator:     '', // The color of the line separating the bottom of the toolbar from the region below.
+        toolbar_field:                '', // The background color for fields in the toolbar, such as the search field.
+        toolbar_field_border:         '', // The border color for fields in the toolbar.
+        toolbar_field_border_focus:   '', // The focused border color for fields in the toolbar.
+        toolbar_field_focus:          '', // The focused background color for fields in the toolbar.
+        toolbar_field_highlight:      '', // The background color used to indicate the current selection of text in the search field.
+        toolbar_field_highlight_text: '', // The color used to draw text that’s currently selected in the search field.
+        toolbar_field_text:           '', // The text color for fields in the toolbar.
+        toolbar_field_text_focus:     '', // The text color in the focused fields in the toolbar.
+        toolbar_text:                 '', // The text color in the main Thunderbird toolbar. Also used as default value for icons and tab_text.
+        toolbar_top_separator:        '', // The color of the line separating the top of the toolbar from the region above.
+        toolbar_vertical_separator:   '', // The color of the vertical separators on the toolbars.
+    },
+
+    theme_experiment_colors: {
+        // ///////////////
+        // //  GENERAL  //
+        // ///////////////
+
+        // '--primary': '', // used as progress bar color
+        // '--primary-color': '',
+        // '--button-primary-text-color':             '',
+        // '--button-primary-border-color':           '',
+        // '--button-primary-background-color':       '',
+        // '--button-primary-hover-background-color': '',
+
+        // '--tree-view-bg':                     '', // inbox list
+        // '--tree-view-header-hover-bg':        '',
+        // '--tree-view-header-hover-active-bg': '',
+        // '--bg-color': '',
+
+        // '--layout-background-0': '',
+        // '--layout-background-1': '',
+        // '--layout-background-2': '',
+        // '--layout-color-1': '',
+
+        // '--new-folder-color': '', // used as NEW email subject color on inbox's cards-view
+
+        // // Main sidebar with main icons
+        // '--spaces-bg-color':                 '',
+        // '--spaces-button-active-text-color': '',
+        // '--spaces-button-active-bg-color':   '',
+
+        // ////////////////
+        // // EMAIL VIEW //
+        // ////////////////
+
+        // // Accounts list - Folder icons
+        // '--folder-color-inbox':         '',
+        // '--folder-color-draft':         '',
+        // '--folder-color-sent':          '',
+        // '--folder-color-archive':       '',
+        // '--folder-color-spam':          '',
+        // '--folder-color-trash':         '',
+        // '--folder-color-template':      '',
+        // '--folder-color-newsletter':    '',
+        // '--folder-color-rss':           '',
+        // '--folder-color-outbox':        '',
+        // '--folder-color-folder':        '',
+        // '--folder-color-folder-filter': '',
+        // '--folder-color-folder-rss':    '',
+
+        // // Accounts list - Email count badges
+        // '--folderpane-unread-new-count-background': '',
+        // '--folderpane-unread-count-text':           '',
+        // '--folderpane-unread-count-background':     '',
+
+        // // Inbox
+        // '--thread-pane-flag-fill':     '', // starred
+        // '--thread-pane-flag-stroke':   '', // starred
+        // '--thread-pane-spam-fill':     '', // span/junk
+        // '--thread-pane-spam-stroke':   '', // span/junk
+        // '--thread-pane-unread-fill':   '', // unread
+        // '--thread-pane-unread-stroke': '', // unread
+
+        // // Account - manage
+        // '--accounts-bg-color':             '',
+        // '--button-hover-background-color': '', // text button hover
+        // '--primary-color-hover':           '', // text-button hover
+        // '--btn-bg':                        '', // normal button
+        // '--btn-bg-hover':                  '', // normal button hover
+
+        // // Calendar
+        // '--viewHighlightBorderColor':     '', // selected day
+        // '--viewDayBoxSelectedBackground': '', // selected day
+        // '--viewTodayLabelColor':          '',
+        // '--viewTodayLabelBackground':     '',
+    },
+};
